32f2f3d6169c22c9bea247e34a4efcba8377bd5b
badripokhrel77 <badripokhrel.77@gmail.com>
2024-08-20 18:39:07 +0545

    push

--- model/index.js
+++ model/index.js
@@ -2,5 +2,17 @@ const {Sequelize,DataType} = required('sequelize')
 
 const sequelize = new Sequelize('nodejs','root','',{
     host:'localhost',
-    port: 3306
+    port: 3306,
+    dialect: 'mysql'
 })
+sequelize.authentication()
+.then(()=>{
+    console.log("correct good!!")
+})
+.catch((err)=>{
+    console.log("Milena tw k bigryo",err)
+})
+const db={}
+db.Sequelize=Sequelize
+db.sequelize=sequelize
+
